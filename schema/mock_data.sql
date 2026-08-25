ee35c8fd187bfefefe8d92f4787522f630b95636
abbi portwood <apwood09@gmail.com>
2026-08-25 17:52:48 -0400

    inital commit

---
+++ schema/mock_data.sql
@@ -0,0 +1,17 @@
+/* mix of "good data" & "bad data" */
+/* test framework's limits */
+
+-- 1. NULL first_name -> test NULL count 
+INSERT INTO EMPLOYEES (employee_id, first_name, email, age)
+VALUES (4, NULL, '', 30); 
+
+-- 2. age -27 -> test range validation 
+INSERT INTO EMPLOYEES (employee_id, first_name, email, age)
+VALUES (2, 'Sarah', '', -27); 
+
+-- 3. employee_id 4 -> test duplicate key 
+INSERT INTO EMPLOYEES (employee_id, first_name, email, age)
+VALUES (4, 'Jaine', '', 20); 
+
+-- save all three inserts permanetly to db 
+COMMIT; 
