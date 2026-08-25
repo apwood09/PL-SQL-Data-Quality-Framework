27bdfc9837b596b67af7af26146287fedd564510
abbi portwood <apwood09@gmail.com>
2026-08-25 18:06:42 -0400

    added comments

--- schema/tables/mock_source.sql
+++ schema/tables/mock_source.sql
@@ -1,9 +1,14 @@
 /* simple test table */
 /* NOT NULL not used -> allow bad data for testing */
 
+-- create EMPLOYEES table 
 create EMPLOYEES table (
+    -- unique id each employee
     employee_id NUMBER, 
+    -- employee's name (text data)
     first_name VARCHAR2, 
+    -- employee's email (text data)
     email VARCHAR2, 
+    -- employee's age (numeric data)
     age NUMBER, 
 ); 
